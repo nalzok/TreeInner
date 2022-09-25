39eed9f85bcbbba24e075bbae36524139616fc72
Qingyao Sun <qys@uchicago.edu>
2022-09-24 23:42:50 -0500

    refactoring

---
+++ src/importance.py
@@ -0,0 +1,192 @@
+from typing import Dict, Tuple
+from time import time
+
+import numpy as np
+import pandas as pd
+from scipy.special import expit
+from sklearn.metrics import mean_squared_error, roc_auc_score
+import xgboost as xgb
+
+
+transformation = {
+    "reg:squarederror": lambda margins: margins,
+    "reg:linear": lambda margins: margins,
+    "binary:logitraw": expit,
+    "reg:logistic": expit,
+    "binary:logistic": expit,
+}
+
+
+def feature_importance(
+    dtrain: xgb.DMatrix,
+    dimportance: xgb.DMatrix,
+    param: Dict,
+    num_boost_round: int,
+    algo: str,
+) -> Tuple[np.ndarray, float]:
+    time_begin = time()
+
+    # reset the margin of dtrain & dimportance
+    dtrain.set_base_margin([])
+    dimportance.set_base_margin([])
+
+    # train a boosting forest with DTRAIN, use it to make prediction for dimportance,
+    # and decompose the result into feature contributions
+    contributions_by_tree = _compute_contribution(
+        dtrain, dimportance, param, num_boost_round, algo
+    )
+
+    # compute gradient
+    gradient_by_tree = _compute_gradient(
+        dimportance,
+        contributions_by_tree,
+        param["objective"],
+        param["base_score"],
+        num_boost_round,
+    )
+    gradient_by_tree = gradient_by_tree[:, :, np.newaxis]
+
+    MDI = np.sum(contributions_by_tree * gradient_by_tree, (0, 1))
+    MDI = MDI[:-1] / param["eta"]
+
+    return MDI, time() - time_begin
+
+
+def _compute_contribution(
+    dtrain: xgb.DMatrix,
+    dimportance: xgb.DMatrix,
+    param: Dict,
+    num_boost_round: int,
+    algo: str,
+) -> np.ndarray:
+    # store the feature contribution of each tree
+    contributions_by_tree = np.zeros(
+        (num_boost_round, dimportance.num_row(), dimportance.num_col() + 1),
+        dtype=np.float32,
+    )
+
+    for t in range(num_boost_round):
+        # train a new tree
+        bst = xgb.train(param, dtrain, 1, verbose_eval=False)
+
+        # compute the contribution_by_tree
+        if algo == "Saabas":
+            pvalid_tree = bst.predict(
+                dimportance,
+                pred_contribs=True,
+                approx_contribs=True,
+                reg_lambda=param["reg_lambda"],
+            )
+        elif algo == "ApproxSHAP":
+            pvalid_tree = bst.predict(
+                dimportance,
+                pred_contribs=True,
+                approx_contribs=True,
+                reg_lambda=0,
+            )
+        elif algo == "SHAP":
+            pvalid_tree = bst.predict(dimportance, pred_contribs=True)
+        else:
+            raise ValueError(f"Unknown algorithm {algo}")
+
+        contributions_by_tree[t, :, :-1] = pvalid_tree[:, :-1]
+        if t == 0:
+            contributions_by_tree[t, :, -1] = pvalid_tree[:, -1]
+        else:
+            contributions_by_tree[t, :, -1] = pvalid_tree[:, -1] - np.sum(
+                contributions_by_tree[:t, :, :], axis=(0, 2)
+            )
+
+        # output the prediction decomposition using all the available trees
+        ptrain_tree = bst.predict(dtrain, output_margin=True)
+
+        # reset the margin to incorporate all the previous trees' prediction
+        dtrain.set_base_margin(ptrain_tree)
+        dimportance.set_base_margin(np.sum(pvalid_tree, axis=1))
+
+    return contributions_by_tree
+
+
+def _compute_gradient(
+    dimportance: xgb.DMatrix,
+    contributions_by_tree: np.ndarray,
+    objective: str,
+    base_score: float,
+    num_boost_round: int,
+) -> np.ndarray:
+    # compute gradient for each tree
+    gradient_by_tree = np.zeros(
+        (num_boost_round, dimportance.num_row()), dtype=np.float32
+    )
+    gradient_by_tree[0, :] = dimportance.get_label() - base_score
+
+    # TODO: is this correct for classification?
+    trans = transformation[objective]
+    for t in range(1, num_boost_round):
+        gradient_by_tree[t, :] = dimportance.get_label() - trans(
+            np.sum(contributions_by_tree[:t, :, :], axis=(0, 2))
+        )
+
+    return gradient_by_tree
+
+
+def permutation_importance(
+    dtrain: xgb.DMatrix,
+    X_valid: pd.DataFrame,
+    Y_valid: pd.DataFrame,
+    param: Dict,
+    num_boost_round: int,
+    perm_round: int,
+) -> Tuple[np.ndarray, float]:
+    time_begin = time()
+
+    # train a boosting forest with DTRAIN, which will be used to make prediction for dimportance
+    bst = xgb.train(param, dtrain, num_boost_round, verbose_eval=False)
+
+    # build DMatrix from data frames
+    dimportance = xgb.DMatrix(X_valid, Y_valid, silent=True)
+
+    # calculate baseline prediction accuracy
+    predictions = bst.predict(dimportance)
+    if param["objective"] in ("reg:squarederror", "reg:linear"):
+        baseline = mean_squared_error(dimportance.get_label(), predictions)
+    else:
+        baseline = 1 - roc_auc_score(dimportance.get_label(), predictions)
+
+    importances = np.zeros((len(X_valid.columns),), dtype=np.float32)
+    for idx, feature in enumerate(X_valid.columns):
+        # permute row and re-calculate prediction accuracy
+        loss_sum = 0
+        for _ in range(perm_round):
+            save = X_valid[feature].copy()
+            X_valid[feature] = np.random.permutation(X_valid[feature])
+            dimportance = xgb.DMatrix(X_valid, Y_valid, silent=True)
+            X_valid[feature] = save
+
+            predictions = bst.predict(dimportance)
+            if param["objective"] in ("reg:squarederror", "reg:linear"):
+                loss_sum += mean_squared_error(dimportance.get_label(), predictions)
+            else:
+                loss_sum += 1 - roc_auc_score(dimportance.get_label(), predictions)
+
+        importances[idx] = baseline - loss_sum / perm_round
+
+    return importances, time() - time_begin
+
+
+def validate_total_gain(
+    candidate: np.ndarray, dtrain: xgb.DMatrix, param: Dict, num_boost_round: int
+) -> float:
+    dtrain.set_base_margin([])
+
+    bst_all_in_one = xgb.train(param, dtrain, num_boost_round, verbose_eval=False)
+    assert bst_all_in_one.feature_names is not None
+    total_gain_dict = bst_all_in_one.get_score(importance_type="total_gain")
+    total_gain = np.array(
+        [total_gain_dict.get(f, 0) for f in bst_all_in_one.feature_names]
+    )
+
+    total_gain /= np.sum(total_gain)
+    candidate /= np.sum(candidate)
+
+    return np.max(np.abs(total_gain - candidate))
---
+++ src/main.py
@@ -0,0 +1,186 @@
+from pathlib import Path
+
+import xgboost as xgb
+import numpy as np
+import pandas as pd
+from tqdm import tqdm, trange
+from sklearn.metrics import roc_auc_score
+import seaborn as sns
+from .importance import (
+    feature_importance,
+    permutation_importance,
+    validate_total_gain,
+)
+
+
+def main(data_root, param, num_boost_rounds):
+    for name in ["max_depth", "eta", "base_score", "objective", "reg_lambda"]:
+        assert name in param, "{} should be in param.".format(name)
+
+    param_str = "+".join(k + "=" + str(v) for k, v in param.items()).replace(".", "p")
+
+    oracle_auc_row = []
+    mdi_error_row = []
+
+    for subproblem in ("classification", "regression"):
+        param["objective"] = (
+            "binary:logistic" if subproblem == "classification" else "reg:squarederror"
+        )
+        for subproblem_id in (1, 2):
+            print(f"Working on {subproblem}{subproblem_id}")
+            subdirectory = data_root / f"{subproblem}{subproblem_id}"
+            for i in trange(40):
+                experiment(
+                    subdirectory,
+                    param,
+                    num_boost_rounds,
+                    subproblem,
+                    subproblem_id,
+                    i,
+                    oracle_auc_row,
+                    mdi_error_row,
+                )
+
+    results = Path("results")
+    results.mkdir(parents=True, exist_ok=True)
+
+    oracle_auc = pd.DataFrame(oracle_auc_row)
+    oracle_auc.to_csv(results / f"oracle-auc+{param_str}.csv")
+
+    mdi_error = pd.DataFrame(mdi_error_row)
+    mdi_error.to_csv(results / f"mdi-error+{param_str}.csv")
+
+    visualize(results, param_str)
+
+
+def experiment(
+    subdirectory,
+    param,
+    num_boost_rounds,
+    subproblem,
+    subproblem_id,
+    i,
+    oracle_auc_row,
+    mdi_error_row,
+):
+    X_train = pd.read_csv(subdirectory / f"permuted{i}_X_train.csv", header=None)
+    Y_train = pd.read_csv(subdirectory / f"permuted{i}_y_train.csv", header=None)
+    X_valid = pd.read_csv(subdirectory / f"permuted{i}_X_test.csv", header=None)
+    Y_valid = pd.read_csv(subdirectory / f"permuted{i}_y_test.csv", header=None)
+    noisy = pd.read_csv(subdirectory / f"permuted{i}_noisy_features.csv", header=None)
+
+    signal = -noisy.round(0).astype(int)
+    # build DMatrix from data frames
+    dtrain = xgb.DMatrix(X_train, Y_train, silent=True)
+    dvalid = xgb.DMatrix(X_valid, Y_valid, silent=True)
+
+    for num_boost_round in tqdm(num_boost_rounds, leave=False):
+        common = {
+            "subproblem": subproblem,
+            "subproblem_id": subproblem_id,
+            "dataset_id": i,
+            "num_boost_round": num_boost_round,
+        }
+
+        total_gain = None
+        for algo in ("Saabas", "ApproxSHAP", "SHAP"):
+            for oob in (False, True):
+                dimportance = dvalid if oob else dtrain
+                score, elapsed = feature_importance(
+                    dtrain, dimportance, param, num_boost_round, algo
+                )
+                if algo == "Saabas" and oob is False:
+                    total_gain = score
+
+                domain = "oob" if oob else "ib"
+                oracle_auc_row.append(
+                    {
+                        "method": f"TotalGain-{algo}-{domain}",
+                        "auc_noisy": roc_auc_score(signal, score),
+                        "elapsed": elapsed,
+                        **common,
+                    }
+                )
+
+        score, elapsed = permutation_importance(
+            dtrain, X_valid, Y_valid, param, num_boost_round, 5
+        )
+        oracle_auc_row.append(
+            {
+                "method": "Permutation (perm_round = 5)",
+                "auc_noisy": roc_auc_score(signal, score),
+                "elapsed": elapsed,
+                **common,
+            }
+        )
+
+        error = validate_total_gain(total_gain, dtrain, param, num_boost_round)
+        mdi_error_row.append(
+            {
+                "error": error,
+                **common,
+            }
+        )
+
+
+def visualize(results, param_str):
+    oracle_auc = pd.read_csv(results / f"oracle-auc+{param_str}.csv")
+    sns_plot = sns.catplot(
+        x="num_boost_round",
+        y="auc_noisy",
+        col="subproblem",
+        row="subproblem_id",
+        hue="method",
+        kind="box",
+        data=oracle_auc,
+    )
+    sns_plot.savefig(f"results/oracle-auc+{param_str}.png")
+
+    oracle_auc["log10(elapsed)"] = np.log10(oracle_auc["elapsed"])
+    sns_plot = sns.catplot(
+        x="num_boost_round",
+        y="log10(elapsed)",
+        col="subproblem",
+        row="subproblem_id",
+        hue="method",
+        kind="box",
+        data=oracle_auc,
+    )
+    sns_plot.savefig(f"results/oracle-elapsed+{param_str}.png")
+
+    mdi_error = pd.read_csv(f"results/mdi-error+{param_str}.csv")
+    mdi_error["log10(error)"] = np.log10(mdi_error["error"])
+
+    sns_plot = sns.catplot(
+        x="num_boost_round",
+        y="log10(error)",
+        col="subproblem",
+        row="subproblem_id",
+        kind="box",
+        palette=sns.color_palette("Blues", n_colors=len(num_boost_rounds)),
+        data=mdi_error,
+    )
+
+    sns_plot.savefig(f"results/mdi-error+{param_str}.png")
+
+
+if __name__ == "__main__":
+    assert (
+        xgb.__version__ == "1.6.2-dev"
+    ), "A custom fork of XGBoost is required. See https://github.com/nalzok/xgboost/tree/release_1.6.0"
+
+    np.random.seed(42)
+    sns.set(style="whitegrid")
+
+    data_root = Path("04_aggregate")
+
+    param = {
+        "max_depth": 5,
+        "eta": 0.1,
+        "base_score": 0.5,
+        "reg_lambda": 1,
+    }
+
+    num_boost_rounds = (1, 2, 4, 8, 16, 32, 64, 128)
+
+    main(data_root, param, num_boost_rounds)
